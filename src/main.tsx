98a0720bf5666e5d247220057661a14094eaade8
A_benaun <Andrey.kuhtin126@gmail.com>
2024-12-08 15:15:08 +0300

    deploy vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import { Provider } from 'react-redux'
 import { store } from './store/store.tsx'
@@ -11,9 +11,9 @@ import './index.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
 )
